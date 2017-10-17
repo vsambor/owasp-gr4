dea3e8857945cbc6e61a0a1b68277a6b5f5a99d7
vsambor <vasilesambor@gmail.com>
2017-10-17 15:14:55 +0200

    Add default user populator

--- include/sql.inc.php
+++ include/sql.inc.php
@@ -35,4 +35,21 @@ function getUser($email, $password) {
   return $stmt->fetch(PDO::FETCH_ASSOC);
 }
 
+/**
+ * Populates the the with 2 default user, with different roles.
+ */
+function populateDb() {
+  // Adds secret user if it does not exist in the db.
+  $secret_exist = getUser('secret@security.com', '123');
+  if(!$secret_exist) {
+    insertUser('secret@security.com', '123', 'secretholder', true);
+  }
+
+  // Adds simple user if it does not exist in the db.
+  $user_exist = getUser('user@security.com', '123');
+  if(!$user_exist) {
+    insertUser('user@security.com', '123', 'user', true);
+  }
+}
+
 ?>
--- include/start.inc.php
+++ include/start.inc.php
@@ -6,3 +6,4 @@ require_once '/include/functions.inc.php';
 require_once '/include/sql.inc.php';
 
  $LOGGED_USER = (isset($_SESSION['user']) && (isset($_SESSION['user']['id']))) ? $_SESSION['user'] : '';
+ populateDb();
