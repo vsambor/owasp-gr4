462d627c98c2f4519ee9b9fbe2f51e7686f5fb07
Vasile Sambor <vasilesambor@gmail.com>
2017-10-26 20:59:07 +0200

    Add secure cookie (httponly)

--- config.php
+++ config.php
@@ -1,13 +1,29 @@
 <?php
+  
+  /**
+   * Database
+   */
   $SETTINGS['db']['host'] = 'localhost';
   $SETTINGS['db']['port'] = 3306;  
   $SETTINGS['db']['name'] = 'owasp';
   $SETTINGS['db']['user'] = 'root';
   $SETTINGS['db']['pass'] = '';
 
-  $SETTINGS['site_host'] = 'http://localhost:8888';
+  /**
+   * Site paths
+   */
+  $SETTINGS['site_host'] = 'http://localhost';
   $SETTINGS['site_path'] = '/security/owasp-gr4';
   $SETTINGS['site_url'] = $SETTINGS['site_host'] . $SETTINGS['site_path'];
-
   $PATH = $_SERVER['DOCUMENT_ROOT'] . $SETTINGS['site_path'];
+
+  /**
+   * Cookies config.
+   */
+  $SESSION_COOKIE['lifetime'] = 1800; //seconds
+  $SESSION_COOKIE['path'] = '/';
+  $SESSION_COOKIE['domain'] = 'localhost';
+  $SESSION_COOKIE['secure'] = isset($_SERVER["HTTPS"]);
+  $SESSION_COOKIE['httponly'] = true;
+
 ?>
--- include/start.inc.php
+++ include/start.inc.php
@@ -2,12 +2,19 @@
 
 ob_start();
 
+session_set_cookie_params(
+  $SESSION_COOKIE['lifetime'],
+  $SESSION_COOKIE['path'],
+  $SESSION_COOKIE['domain'],
+  $SESSION_COOKIE['secure'],
+  $SESSION_COOKIE['httponly']
+);
 session_start();
 
 require_once $PATH . '/include/db.inc.php';
 require_once $PATH . '/include/functions.inc.php';
 require_once $PATH . '/include/sql.inc.php';
 
- $LOGGED_USER = (isset($_SESSION['user']) && (isset($_SESSION['user']['id']))) ? $_SESSION['user'] : '';
+$LOGGED_USER = (isset($_SESSION['user']) && (isset($_SESSION['user']['id']))) ? $_SESSION['user'] : '';
 
- populateDb();
+populateDb();
