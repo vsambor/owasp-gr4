a1fb3458e8a560631add69b199170a4c8005e94e
Alicia Vílchez Ceballos <aliavc96@correo.ugr.es>
2017-10-29 16:24:10 +0100

    Added a security function to force users to use a strong password policy

--- include/functions.inc.php
+++ include/functions.inc.php
@@ -24,4 +24,26 @@ function isEmailValid($email) {
   return filter_var($email, FILTER_VALIDATE_EMAIL);
 }
 
+function isPasswordValid($password, &$error){
+   if(strlen($password) < 6){
+      $error = "Password must be at least 6 characters";
+      return false;
+   }
+  
+   if (!preg_match('`[a-z]`',$password)){
+      $error = "Password must have at least one lower case letter";
+      return false;
+   }
+   if (!preg_match('`[A-Z]`',$password)){
+      $error = "Password must have at least one uppercase letter";
+      return false;
+   }
+   if (!preg_match('`[0-9]`',$password)){
+      $error = "Password must have at least one numeric character";
+      return false;
+   }
+   $error = "";
+   return true;
+} 
+
 ?>
